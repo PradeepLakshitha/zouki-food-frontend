2d3a97cdc39a73614ed32b5e317db6040d23ad29
pradeeplakshitha <jpradeeplakshitha@gmail.com>
2024-02-14 16:46:38 +1100

    test loading logo

--- src/Product.js
+++ src/Product.js
@@ -6,6 +6,7 @@ import QRCode from "qrcode.react";
 
 // Import the logo image
 import logo from "./logo.png";
+import loadingGif from "./loading-loader.gif"; // Import your loading GIF file
 
 const ProductList = () => {
   const { id } = useParams();
@@ -13,6 +14,7 @@ const ProductList = () => {
   const [zoomedImage, setZoomedImage] = useState(null);
   const [rotation, setRotation] = useState(0);
   const [zoom, setZoom] = useState(1);
+  const [loading, setLoading] = useState(true); // State for loading animation
 
   useEffect(() => {
     fetchProducts();
@@ -25,6 +27,7 @@ const ProductList = () => {
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
+        setLoading(false); // Once data is fetched, stop loading animation
       })
       .catch((error) => console.error("Error fetching data:", error));
   };
@@ -64,65 +67,67 @@ const ProductList = () => {
 
   return (
     <div>
-      <div className="container">
-        <div className="grid-container">
-          {products?.map((product) => (
-            <div key={product.productID} className="product-card">
-              {/* Logo image */}
-              <center>
-                <img src={logo} alt="Logo" className="product-logo" />
-              </center>
-              <div className="product-image-container">
-                {/* Product image */}
-                <img
-                  src={product.imgUrl}
-                  alt={product.proName}
-                  className="product-image"
-                  onClick={() => handleImageClick(product.imgUrl)}
-                />
-                <div className="product-name">{product.proName}</div>
-              </div>
-              <div className="details-container">
-                <div className="subheader">
-                  <strong>Ingredients:</strong>
-                </div>
-                <div className="ingredients">{product.ing}</div>
-                <div className="subheader">
-                  <strong>Allergens:</strong>
-                </div>
-                <div className="allergens">{product.aller}</div>
-              </div>
-
-              <div className="disclaimer">
-                <p style={{ color: "black", fontWeight: "bold" }}>DISCLAIMER:</p>
-                <p style={{ color: "red"}}>
-                  This product is produced in a facility where the environment
-                  contains milk, peanuts, sesame, soy, tree nuts, gluten, lupin,
-                  crustacean & fish. Although the safest methods are
-                  implemented, accidental or unintentional cross-contamination
-                  may occur. Therefore, please be aware that we cannot
-                  completely guarantee the absence of undeclared allergens.
-                </p>
-              </div>
-
-              <div className="qr-code">
-                <QRCode
-                  value={`https://main.d3nnq1ywmugn1z.amplifyapp.com/product/${product.productID}`}
-                  size={128}
-                  level="H"
-                  onScan={handleQRCodeScan}
-                  imageSettings={{
-                    src: logo, // Replace 'path_to_your_logo' with the path to your logo
-                    excavate: true,
-                    width: 80,
-                    height: 30, // Keep true to crop the image in the shape of QR Code
-                  }}
-                />
-              </div>
-            </div>
-          ))}
+      {loading ? (
+        <div className="loading-animation">
+          <img src={loadingGif} alt="Loading..." />
         </div>
-      </div>
+      ) : (
+        <div className="container">
+          <div className="grid-container">
+            {products?.map((product) => (
+              <div key={product.productID} className="product-card">
+                <center>
+                  <img src={logo} alt="Logo" className="product-logo" />
+                </center>
+                <div className="product-image-container">
+                  <img
+                    src={product.imgUrl}
+                    alt={product.proName}
+                    className="product-image"
+                    onClick={() => handleImageClick(product.imgUrl)}
+                  />
+                  <div className="product-name">{product.proName}</div>
+                </div>
+                <div className="details-container">
+                  <div className="subheader">
+                    <strong>Ingredients:</strong>
+                  </div>
+                  <div className="ingredients">{product.ing}</div>
+                  <div className="subheader">
+                    <strong>Allergens:</strong>
+                  </div>
+                  <div className="allergens">{product.aller}</div>
+                </div>
+                <div className="disclaimer">
+                  <p style={{ color: "black", fontWeight: "bold" }}>DISCLAIMER:</p>
+                  <p style={{ color: "red"}}>
+                    This product is produced in a facility where the environment
+                    contains milk, peanuts, sesame, soy, tree nuts, gluten, lupin,
+                    crustacean & fish. Although the safest methods are
+                    implemented, accidental or unintentional cross-contamination
+                    may occur. Therefore, please be aware that we cannot
+                    completely guarantee the absence of undeclared allergens.
+                  </p>
+                </div>
+                <div className="qr-code">
+                  <QRCode
+                    value={`https://main.d3nnq1ywmugn1z.amplifyapp.com/product/${product.productID}`}
+                    size={128}
+                    level="H"
+                    onScan={handleQRCodeScan}
+                    imageSettings={{
+                      src: logo,
+                      excavate: true,
+                      width: 80,
+                      height: 30,
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
       {zoomedImage && (
         <div className="zoomed-image-container">
           <div className="zoomed-image">
